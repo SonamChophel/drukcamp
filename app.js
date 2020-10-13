98d17b0376d447e012728ecc10aed87589bba518
Chophel98 <chophel9808@gmail.com>
2020-10-13 16:22:21 +0600

    port set

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comment",commentRoutes);
 
-app.listen(8080 , function(){
+app.listen( process.env.PORT || 8080 , function(){
     console.log("Server has started!!!");
 });
